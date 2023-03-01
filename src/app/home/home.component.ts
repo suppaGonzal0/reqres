17307204f0eb242f86a7cd25e9a1c0b5d36f43d9
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-03-01 23:19:19 +0600

    separated home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  users = []
 
+  constructor(private http: HttpClient, private router: Router){}
+
+  ngOnInit(){
+    this.fetchUsers(1)
+  }
+
+  fetchUsers(pageNum: number){
+    this.http.get(`https://reqres.in/api/users?page=${pageNum}`)
+    .subscribe((response: any) => {
+      this.users = response.data
+    })
+  }
+
+  showUserInfo(user: any){
+    this.router.navigateByUrl(`/${user.id}`)
+  }
+
+  editUser(){
+    alert("User is about to be edited")
+  }
+
+  deleteUser(id: number){
+    alert(`You are about to delete user with ID: ${id}`) 
+  }
 }
