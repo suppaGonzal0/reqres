9135328b22d7dbc1ac7a3155c066a4af987ad1f7
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-03-05 23:37:25 +0600

    register component

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,17 @@
+import { FirebaseService } from '../services/firebase.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  constructor(private firebaseService: FirebaseService){}
+
+  registerUser(registerUser: any){
+    // console.log(typeof registerUser)
+    this.firebaseService.register(registerUser.email, registerUser.password)
+  }
+}
