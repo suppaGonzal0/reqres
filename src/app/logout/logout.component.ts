27ae9844f198a96c0cdeda5ccc35f905945e479a
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-03-05 23:37:12 +0600

    logout component

---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,17 @@
+import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent {
+
+  constructor(private firebaseService: FirebaseService){}
+
+  logout(){
+    this.firebaseService.logout()
+  }
+}
